aa214c591790159a359ddb6ca721bb7b7573dc93
Kacper Pogodziński <kpogodzinski@mat.umk.pl>
2025-03-17 13:18:28 +0100

    Dodanie korutyny do zmiany koloru. Utworzenie list sąsiedztwa dla grafu. Poprawa skryptu DFS. Zmiana struktury obiektów na scenie.

--- Assets/Scripts/DFS_BFS.cs
+++ Assets/Scripts/DFS_BFS.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -17,128 +19,66 @@ public class DFS_BFS : MonoBehaviour
     private string searchType;
     [SerializeField]
     private float timeout;
+    [SerializeField]
+    private float animDuration;
+    [SerializeField]
+    private GameObject nodes;
+    [SerializeField]
+    private GameObject edges;
 
-    private List<GameObject> nodes = new();
-    private List<GameObject> edges = new();
-    private bool[][] matrix;
+    private List<GameObject> nodesList = new();
+    private List<GameObject> edgesList = new();
+
+    private List<List<bool>> matrix = new();
     private bool[] visited;
+    private Dictionary<int, List<int>> neighborsList = new();
 
-    int MarkEdges(int node, Color color)
+    //int MarkEdges(int node, Color color)
+    //{
+    //    int count = 0;
+    //    for (int i = 0; i < numberOfNodes; i++)
+    //    {
+    //        GameObject line = GameObject.Find($"{node}-{i}");
+    //        if (line == null)
+    //            line = GameObject.Find($"{i}-{node}");
+    //        if (line != null)
+    //        {
+    //            line.GetComponent<Renderer>().material.color = color;
+    //            count++;
+    //        }
+    //    }
+    //    return count;
+    //}
+
+    private IEnumerator ChangeColor(GameObject gameObject, Color newColor)
     {
-        int count = 0;
-        for (int i = 0; i < numberOfNodes; i++)
+        Renderer renderer = gameObject.GetComponent<Renderer>();
+        Color currentColor = renderer.material.color;
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < animDuration)
         {
-            GameObject line = GameObject.Find($"{node}-{i}");
-            if (line == null)
-                line = GameObject.Find($"{i}-{node}");
-            if (line != null)
-            {
-                line.GetComponent<Renderer>().material.color = color;
-                count++;
-            }
+            renderer.material.color = Color.Lerp(currentColor, newColor, elapsedTime / animDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
         }
-        return count;
+
+        renderer.material.color = newColor;
     }
 
-    IEnumerator DFS(int start)
+    /// MACIERZ S¥SIEDZTWA TWORZONA RÊCZNIE!!!
+    private void CreateMatrix()
     {
-        yield return new WaitForSeconds(timeout);
-
-        List<int> nodesToVisit = new() { start };
-
-        while (nodesToVisit.Count > 0)
-        {
-            // Go to the next node on the list to visit
-            int n = nodesToVisit.First();
-            var node = nodes[n];
-            nodesToVisit.Remove(n);
-            node.transform.localScale = 2 * Vector3.one;
-            yield return new WaitForSeconds(timeout);
-
-            // Set the node as visited
-            visited[n] = true;
-            node.GetComponent<Renderer>().material.color = Color.green;
-            yield return new WaitForSeconds(timeout);
-
-            // Check all neighbors of the current node
-            MarkEdges(n, Color.red);
-            for (int i = numberOfNodes - 1; i >= 0; i--)
-            {
-                if (matrix[n][i] || matrix[i][n])
-                { 
-                    // If the neighbor was not visited, add it to the visit list (if does not exist yet)
-                    if (!visited[i])
-                        if (!nodesToVisit.Contains(i))
-                            nodesToVisit = nodesToVisit.Prepend(i).ToList();
-                }
-            }
-            yield return new WaitForSeconds(timeout);
-
-            node.transform.localScale = Vector3.one;
-        }
-    }
-
-    IEnumerator BFS(int start)
-    {
-        yield return new WaitForSeconds(timeout);
-
-        List<int> nodesToVisit = new() { start };
-
-        while (nodesToVisit.Count > 0)
-        {
-            // Go to the next node on the list to visit
-            int n = nodesToVisit.First();
-            GameObject node = nodes[n];
-            nodesToVisit.Remove(n);
-            node.transform.localScale = 2 * Vector3.one;
-            yield return new WaitForSeconds(timeout);
-
-            // Set the node as visited
-            visited[n] = true;
-            node.GetComponent<Renderer>().material.color = Color.green;
-            yield return new WaitForSeconds(timeout);
-
-            // Check all neighbors of the current node
-            MarkEdges(n, Color.red);
-            for (int i = 0; i < numberOfNodes; i++)
-            {
-                if (matrix[n][i] || matrix[i][n])
-                {
-                    // If the neighbor was not visited, add it to the visit list (if does not exist yet)
-                    if (!visited[i])
-                        if (!nodesToVisit.Contains(i))
-                            nodesToVisit = nodesToVisit.Append(i).ToList();
-                }
-            }
-            yield return new WaitForSeconds(timeout);
-
-            node.transform.localScale = Vector3.one;
-        }
-    }
-
-    void Start()
-    {
-        visited = new bool[numberOfNodes];
-        matrix = new bool[numberOfNodes][];
-        for (int i = 0; i < numberOfNodes; i++)
-            matrix[i] = new bool[numberOfNodes];
         for (int i = 0; i < numberOfNodes; i++)
         {
-            nodes.Add(GameObject.Find($"Node{i}"));
-            nodes[i].transform.GetChild(0).GetComponent<TextMeshPro>().text = (i+1).ToString();
-            nodes[i].transform.GetChild(1).GetComponent<TextMeshPro>().text = (i+1).ToString();
+            matrix.Add(new());
+            for (int j = 0; j < numberOfNodes; j++)
+            {
+                matrix[i].Add(false);
+            }
         }
 
-        //// Create sphere objects that represent the nodes
-        //for (int i = 0; i < numberOfNodes; i++)
-        //{
-        //    Vector3 position = new Vector3(-numberOfNodes/2 + i, )
-        //    nodes.Add(Instantiate(prefab, );
-        //    nodes[i].name = $"Node{i}";
-        //    nodes[i].transform.GetChild(0).GetComponent<TextMeshPro>().text = i.ToString();
-        //}
-
-        // Add edges to the matrix
         matrix[0][1] = true;
         matrix[0][4] = true;
         matrix[1][2] = true;
@@ -149,6 +89,117 @@ public class DFS_BFS : MonoBehaviour
         matrix[4][7] = true;
         matrix[5][6] = true;
         matrix[7][8] = true;
+    }
+
+    private void CreateNeighborsList()
+    {
+        for (int i = 0; i < numberOfNodes; i++)
+        {
+            neighborsList.Add(i, new List<int>());
+            for (int j = 0; j < numberOfNodes; j++)
+            {
+                if (matrix[i][j])
+                {
+                    neighborsList[i].Add(j);
+                }
+            }
+        }
+    }
+
+    private IEnumerator DFS(int start)
+    {
+        yield return new WaitForSeconds(timeout);
+
+        Stack<int> stack = new();
+        stack.Push(start);
+
+        while (stack.Count > 0)
+        {
+            // Get next node from the stack
+            int n = stack.Pop();
+
+            // Set the node as visited
+            visited[n] = true;
+            StartCoroutine(ChangeColor(nodesList[n], Color.green));
+            yield return new WaitForSeconds(timeout);
+
+            // Check all neighbors of the current node
+            foreach(int nb in neighborsList[n])
+            {
+                if (!visited[nb] && !stack.Contains(nb))
+                {
+                    StartCoroutine(ChangeColor(nodesList[nb], Color.yellow));
+                    stack.Push(nb);
+                }
+            }
+            
+            //for (int i = numberOfNodes - 1; i >= 0; i--)
+            //{
+            //    if (matrix[n][i] || matrix[i][n])
+            //    { 
+            //        // If the neighbor was not visited, add it to the visit list (if does not exist yet)
+            //        if (!visited[i])
+            //            if (!nodesToVisit.Contains(i))
+            //                nodesToVisit = nodesToVisit.Prepend(i).ToList();
+            //    }
+            //}
+            yield return new WaitForSeconds(timeout);
+
+            //node.transform.localScale = Vector3.one;
+        }
+    }
+
+    //IEnumerator BFS(int start)
+    //{
+    //    yield return new WaitForSeconds(timeout);
+
+    //    List<int> nodesToVisit = new() { start };
+
+    //    while (nodesToVisit.Count > 0)
+    //    {
+    //        // Go to the next node on the list to visit
+    //        int n = nodesToVisit.First();
+    //        GameObject node = nodes[n];
+    //        nodesToVisit.Remove(n);
+    //        node.transform.localScale = 2 * Vector3.one;
+    //        yield return new WaitForSeconds(timeout);
+
+    //        // Set the node as visited
+    //        visited[n] = true;
+    //        node.GetComponent<Renderer>().material.color = Color.green;
+    //        yield return new WaitForSeconds(timeout);
+
+    //        // Check all neighbors of the current node
+    //        MarkEdges(n, Color.red);
+    //        for (int i = 0; i < numberOfNodes; i++)
+    //        {
+    //            if (matrix[n][i] || matrix[i][n])
+    //            {
+    //                // If the neighbor was not visited, add it to the visit list (if does not exist yet)
+    //                if (!visited[i])
+    //                    if (!nodesToVisit.Contains(i))
+    //                        nodesToVisit = nodesToVisit.Append(i).ToList();
+    //            }
+    //        }
+    //        yield return new WaitForSeconds(timeout);
+
+    //        node.transform.localScale = Vector3.one;
+    //    }
+    //}
+
+    void Start()
+    {
+        CreateMatrix();
+        CreateNeighborsList();
+        visited = new bool[numberOfNodes];
+
+        // Dodanie wêz³ów do listy i nadanie etykiet
+        for (int i = 0; i < numberOfNodes; i++)
+        {
+            nodesList.Add(nodes.transform.GetChild(i).gameObject);
+            nodesList[i].transform.GetChild(0).GetComponent<TextMeshPro>().text = (i + 1).ToString();
+            nodesList[i].transform.GetChild(1).GetComponent<TextMeshPro>().text = (i + 1).ToString();
+        }
 
         // Draw lines that connect the nodes
         for (int i = 0; i < numberOfNodes; i++)
@@ -160,8 +211,8 @@ public class DFS_BFS : MonoBehaviour
 
                 if (matrix[i][j])
                 {
-                    from = nodes[i].transform.position;
-                    to = nodes[j].transform.position;
+                    from = nodesList[i].transform.position;
+                    to = nodesList[j].transform.position;
                     line = new() { name = $"{i}-{j}" };
 
                 }
@@ -175,14 +226,14 @@ public class DFS_BFS : MonoBehaviour
                 lr.startWidth = 0.1f;
                 lr.endWidth = 0.1f;
                 line.GetComponent<Renderer>().material.color = Color.white;
-                edges.Add(line);
+                edgesList.Add(line);
             }
         }
 
         if (searchType == "DFS")
             StartCoroutine(DFS(startingNode));
-        else if (searchType == "BFS")
-            StartCoroutine(BFS(startingNode));
+        //else if (searchType == "BFS")
+        //    StartCoroutine(BFS(startingNode));
     }
 
     void Update()
