b0cdcfa21f012eb5fa9ca1aa7118c82341a2d0d7
Wiktoria Zietak <wzietak@mat.umk.pl>
2025-03-19 23:23:10 +0100

    Poprawa wyglądu/wielkosci okienka z opcjami. Poprawa dzialania slidera na dotyk - nadal nie jest idealnie.

--- Assets/UI/Scripts/ClickableSlider.cs
+++ Assets/UI/Scripts/ClickableSlider.cs
@@ -5,10 +5,17 @@ using UnityEngine.EventSystems;
 public class ClickableSlider : MonoBehaviour, IPointerDownHandler
 {
     private Slider slider;
+    private RectTransform handleRect;
 
     void Start()
     {
         slider = GetComponent<Slider>();
+
+        // Pobranie RectTransform z uchwytu (Handle)
+        if (slider.handleRect != null)
+        {
+            handleRect = slider.handleRect;
+        }
     }
 
     public void OnPointerDown(PointerEventData eventData)
@@ -16,6 +23,22 @@ public class ClickableSlider : MonoBehaviour, IPointerDownHandler
         RectTransform sliderRect = slider.GetComponent<RectTransform>();
         Vector2 localPoint;
 
+        // Jeœli klikniêto na Handle, zmieñ wartoœæ suwaka
+        if (handleRect != null && RectTransformUtility.RectangleContainsScreenPoint(handleRect, eventData.position, eventData.pressEventCamera))
+        {
+            SetSliderValue(eventData);
+        }
+        // Jeœli klikniêto na suwak, ale poza Handle
+        else if (RectTransformUtility.ScreenPointToLocalPointInRectangle(sliderRect, eventData.position, eventData.pressEventCamera, out localPoint))
+        {
+            SetSliderValue(eventData);
+        }
+    }
+
+    private void SetSliderValue(PointerEventData eventData)
+    {
+        RectTransform sliderRect = slider.GetComponent<RectTransform>();
+        Vector2 localPoint;
         if (RectTransformUtility.ScreenPointToLocalPointInRectangle(sliderRect, eventData.position, eventData.pressEventCamera, out localPoint))
         {
             float normalizedValue = Mathf.InverseLerp(sliderRect.rect.xMin, sliderRect.rect.xMax, localPoint.x);
