bae2c51e1a58e74cfc199dce00dabfcd5f8f587f
Wiktoria Zietak <wzietak@mat.umk.pl>
2025-04-01 15:24:48 +0200

    Dodanie przełączania pomiędzy krokami i opisami algorytmów. Przygotowanie wersji BottomButtons w Info wyswietlanej gdy uzytkownik przelaczyl sie na kroki. Wrzucenie do steps.txt przykladowego tekstu zeby sprawdzic dzialanie przelaczania

--- Assets/Scripts/ToggleInfo.cs
+++ Assets/Scripts/ToggleInfo.cs
@@ -8,6 +8,7 @@ public class ToggleInfo : MonoBehaviour
     [SerializeField]
     private GameObject info;
     private int algorithmNo;
+    private bool isShowingSteps = false;
    
     private void HideInfo()
     {
@@ -16,6 +17,15 @@ public class ToggleInfo : MonoBehaviour
 
     private void ShowInfo()
     {
+        if (isShowingSteps)
+        {
+            info.transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = Dictionaries.stepBySteps.GetValueOrDefault(algorithmNo).ToString();
+        }
+        else
+        {
+            info.transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = Dictionaries.descriptions.GetValueOrDefault(algorithmNo).ToString();
+        }
+
         info.SetActive(true);
     }
 
@@ -27,10 +37,25 @@ public class ToggleInfo : MonoBehaviour
             ShowInfo();
     }
 
+    public void OnStepsButtonClick()
+    {
+        isShowingSteps = true;
+        info.transform.GetChild(1).gameObject.SetActive(false);
+        info.transform.GetChild(2).gameObject.SetActive(true);
+        ShowInfo();
+    }
+
+    public void OnDescriptionButtonClick()
+    {
+        isShowingSteps = false;
+        info.transform.GetChild(1).gameObject.SetActive(true);
+        info.transform.GetChild(2).gameObject.SetActive(false);
+        ShowInfo();
+    }
+
     private void Start()
     {
         algorithmNo = Dictionaries.algorithms.GetValueOrDefault(PlayerPrefs.GetString("algorithm"));
-        //info.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = Dictionaries.descriptions.GetValueOrDefault(algorithmNo).ToString();
         info.transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = Dictionaries.descriptions.GetValueOrDefault(algorithmNo).ToString();
 
     }
