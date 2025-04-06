d05b14f73a9e4525e2b9b2fba3ca119838a9e6f3
Kacper Pogodziński <kpogodzinski@mat.umk.pl>
2025-04-06 11:34:08 +0200

    Te same poprawki dla BFS.

--- Assets/Scripts/Graphs/BFSGraph.cs
+++ Assets/Scripts/Graphs/BFSGraph.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BFSGraph : Graphs
@@ -13,23 +14,29 @@ public class BFSGraph : Graphs
         yield return new WaitForSeconds(timeout);
 
         Queue<int> queue = new();
-        // Dodajemy wêze³ pocz¹tkowy zarówno do logiki jak i wizualizacji
         queue.Enqueue(startingNode);
         graphQueue.EnqueueVisual(startingNode);
         StartCoroutine(ChangeColor(nodesList[startingNode], Color.yellow));
         yield return new WaitForSeconds(timeout);
-
         List<int> visited = new();
+
         while (queue.Count > 0)
         {
             // Pobieramy element z kolejki i wizualnie go usuwamy
             int n = queue.Dequeue();
             graphQueue.DequeueVisual();
-
-            // Wizualizacja aktualnego wêz³a
             StartCoroutine(ChangeSize(nodesList[n], 1.5f * Vector3.one));
             yield return new WaitForSeconds(timeout);
 
+            // Pominiêcie, jeœli by³ ju¿ odwiedzony
+            if (visited.Contains(n))
+            {
+                StartCoroutine(ChangeSize(nodesList[n], Vector3.one));
+                yield return new WaitForSeconds(timeout);
+                continue;
+            }
+
+            // Oznaczenie wêz³a jako odwiedzonego
             visited.Add(n);
             StartCoroutine(ChangeColor(nodesList[n], Color.green));
             addText(n.ToString());
@@ -46,16 +53,19 @@ public class BFSGraph : Graphs
             yield return new WaitForSeconds(timeout);
 
             // Przetwarzamy s¹siadów wêz³a
-            foreach (int nb in neighborsList[n])
+            if (neighborsList[n].Any(nb => !visited.Contains(nb)))
             {
-                if (!visited.Contains(nb) && !queue.Contains(nb))
+                foreach (int nb in neighborsList[n])
                 {
-                    StartCoroutine(ChangeColor(nodesList[nb], Color.yellow));
-                    queue.Enqueue(nb);
-                    graphQueue.EnqueueVisual(nb);
+                    if (!visited.Contains(nb))
+                    {
+                        StartCoroutine(ChangeColor(nodesList[nb], Color.yellow));
+                        queue.Enqueue(nb);
+                        graphQueue.EnqueueVisual(nb);
+                    }
                 }
+                yield return new WaitForSeconds(timeout);
             }
-            yield return new WaitForSeconds(timeout);
 
             // Przywracamy domyœlny kolor krawêdzi
             foreach (GameObject edge in edgesList)
