e8d18a18b4afff67800d7dd10d9042dba8b45b0c
Qrag11 <m.przystawski8@gmail.com>
2025-04-14 18:58:34 +0200

    Dodanie funkcjonalnosci speed button, zwiekszanie predkosci od 1 do 2 co 0.25

---
+++ Assets/Scripts/Sortings/SpeedSortings.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SpeedButton : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI speedText;
+
+    private float[] speeds = { 1.0f, 1.25f, 1.5f, 1.75f, 2.0f };
+    private int currentSpeedIndex = 0;
+
+    public void OnSpeedButtonClick()
+    {
+        currentSpeedIndex = (currentSpeedIndex + 1) % speeds.Length;
+        Time.timeScale = speeds[currentSpeedIndex];
+
+        if (speedText != null)
+        {
+            speedText.text = $"{speeds[currentSpeedIndex]}x";
+        }
+    }
+
+    void Start()
+    {
+        string algorithm = PlayerPrefs.GetString("algorithm");
+
+        // Poka¿ tylko, jeœli to Sort albo Graph
+        if (algorithm.Contains("Sort") || algorithm.Contains("Graph"))
+        {
+            gameObject.SetActive(true);
+            Time.timeScale = speeds[currentSpeedIndex];
+            if (speedText != null)
+            {
+                speedText.text = $"{speeds[currentSpeedIndex]}x";
+            }
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
