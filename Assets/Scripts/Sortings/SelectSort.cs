5951d88a24144a9b62df158c483f4276a6125352
Leszek <a.wierzchowski@proton.me>
2025-04-17 18:25:51 +0200

    Połączenie (nadgonienie) aktualnego deva oraz aktualizacja skryptu o zdefiniowane dla projektu kolory algorytmów

--- Assets/Scripts/Sortings/SelectSort.cs
+++ Assets/Scripts/Sortings/SelectSort.cs
@@ -1,14 +1,10 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 using System;
 using TMPro;
 
 public class SelectSort : Sortings
 {
-    private int minIndex;
-    private Color defaultColor;
     private int[] heights;
     private int[] checkDuplicate;
     private static int[] saveHeights;
@@ -16,7 +12,6 @@ public class SelectSort : Sortings
 
     void setup()
     {
-        ColorUtility.TryParseHtmlString("#4274b2", out defaultColor);
 
         if (firstTime)
         {
@@ -43,7 +38,7 @@ public class SelectSort : Sortings
             
             items[i].transform.localScale = new Vector3(1, heights[i] * 0.20f, 1);
             items[i].transform.localPosition = new Vector3(items[i].transform.localPosition.x, heights[i] * 0.10f, 0);
-            SetColor(items[i], defaultColor);
+            StartCoroutine(ChangeColor(items[i], blueColor));
 
             float scale = heights[i] * 0.20f;
 
@@ -60,7 +55,6 @@ public class SelectSort : Sortings
             firstTime = false;
         }
         
-
     }
 
     protected override IEnumerator Sort()
@@ -68,36 +62,37 @@ public class SelectSort : Sortings
         setup();
         for (int i = 0; i < numberOfItems-1; i++)
         {
-            SetColor(items[i], Color.red);
+            StartCoroutine(ChangeColor(items[i], redColor));
             yield return new WaitForSeconds(timeout);
             int aktMin = i;
             for (int j = i + 1; j < numberOfItems; j++)
             {
-                SetColor(items[j], Color.yellow);
+                StartCoroutine(ChangeColor(items[j], yellowColor));
+
                 yield return new WaitForSeconds(timeout);
                 if (heights[j] < heights[aktMin])
                 {
-                    SetColor(items[aktMin], defaultColor);
-                    SetColor(items[j], Color.red);
+                    StartCoroutine(ChangeColor(items[aktMin], blueColor));
+                    StartCoroutine(ChangeColor(items[j], redColor));
+
                     aktMin = j;
                     yield return new WaitForSeconds(timeout);
                 }
                 else
                 {
-                    SetColor(items[j], defaultColor);
+                    StartCoroutine(ChangeColor(items[j], blueColor));
                 }
 
             }
             if(aktMin == i)
             {
-                SetColor(items[i], Color.green);
+                StartCoroutine(ChangeColor(items[i], greenColor));
                 yield return new WaitForSeconds(timeout);
             }
             else
             {
-                SetColor(items[i], Color.red);
+                StartCoroutine(ChangeColor(items[i], redColor));
                 yield return new WaitForSeconds(timeout);
-
 
                 int temp = heights[i];
                 heights[i] = heights[aktMin];
@@ -107,7 +102,7 @@ public class SelectSort : Sortings
                 items[i] = items[aktMin];
                 items[aktMin] = tempObject;
 
-                SetColor(items[aktMin], defaultColor);
+                StartCoroutine(ChangeColor(items[aktMin], blueColor));
 
                 Vector3 PointFirst = items[aktMin].transform.localPosition;
                 Vector3 PointSecond = items[i].transform.localPosition;
@@ -130,7 +125,6 @@ public class SelectSort : Sortings
                 time = 0;
                 while (time < animDuration) //zmiana miejsc
                 {
-
                     items[aktMin].transform.localPosition = Vector3.Lerp(PointFirst2, PointSecond2, time / animDuration);
                     items[aktMin].transform.localPosition = new Vector3(items[aktMin].transform.localPosition.x, (items[aktMin].transform.localScale.y / 2), 1);
 
@@ -153,7 +147,6 @@ public class SelectSort : Sortings
 
                 while (time < animDuration) // wsuniêcie 
                 {
-                    
                     items[aktMin].transform.localPosition = Vector3.Lerp(PointFirst2, PointSecond, time / animDuration);
                     items[i].transform.localPosition = Vector3.Lerp(PointSecond2, PointFirst, time / animDuration);
 
@@ -162,17 +155,12 @@ public class SelectSort : Sortings
                 }
                 yield return new WaitForSeconds(timeout/5);
 
-                SetColor(items[i], Color.green);
+                StartCoroutine(ChangeColor(items[i], greenColor));
                 yield return new WaitForSeconds(timeout);
             }
 
-            
         }
-        SetColor(items[numberOfItems - 1], Color.green);
-    }
-    void SetColor(GameObject cube, Color color)
-    {
-        cube.gameObject.GetComponent<MeshRenderer>().material.color = color;
+        StartCoroutine(ChangeColor(items[numberOfItems - 1], greenColor));
     }
 
 
