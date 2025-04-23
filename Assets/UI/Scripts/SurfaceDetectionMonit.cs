85b3d1ee39cd392b05d22deada5c98b74c9a0dbd
Wiktoria Zietak <wzietak@mat.umk.pl>
2025-04-24 00:46:40 +0200

    Dodanie monitu o zeskanowaniu pomieszczenia. Dodanie animacji na ikonę tel. Dodanie skryptu znikającego monit po 3 sek - na razie bez warunku wykrycia powierzchni

---
+++ Assets/UI/Scripts/SurfaceDetectionMonit.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeOutMonit : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+    private float fadeDuration = 2f; 
+
+    private void Start()
+    {
+
+        canvasGroup = GetComponent<CanvasGroup>();
+        StartCoroutine(FadeOut());
+    }
+
+    private IEnumerator FadeOut()
+    {
+        yield return new WaitForSeconds(3f);
+        canvasGroup.alpha = 1f;
+
+        float elapsedTime = 0f;
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(1f, 0f, elapsedTime / fadeDuration);
+            yield return null;
+        }
+
+        canvasGroup.alpha = 0f;
+        gameObject.SetActive(false); 
+    }
+}
